aec0230ce0ca5106ed270470da1e40c8d232b989
corsacca <corsacca@gmail.com>
2019-04-08 11:41:19 +0100

    Plugins: use global functions in theme. Test for D.T version. And prevent plugin loading on all API calls.

--- disciple-tools-starter-plugin.php
+++ disciple-tools-starter-plugin.php
@@ -47,6 +47,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
+$dt_starter_required_dt_theme_version = '0.19.0';
 
 /**
  * Gets the instance of the `DT_Starter_Plugin` class.
@@ -56,17 +57,30 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return object
  */
 function dt_starter_plugin() {
-    $current_theme = wp_get_theme()->get( "Name" );
-
-    if ( 'Disciple Tools' == $current_theme || dt_is_child_theme_of_disciple_tools() ) {
+    global $dt_starter_required_dt_theme_version;
+    $wp_theme = wp_get_theme();
+    $version = $wp_theme->version;
+    /*
+     * Check if the Disciple.Tools theme is loaded and is the latest required version
+     */
+    if ( 'disciple-tools-theme' !== $wp_theme->get_template() || $version < $dt_starter_required_dt_theme_version ) {
+        add_action( 'admin_notices', 'dt_starter_plugin_hook_admin_notice' );
+        add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
+        return new WP_Error( 'current_theme_not_dt', 'Disciple Tools Theme not active or not latest version.' );
+    }
+    /**
+     * Load useful function from the theme
+     */
+    if ( !defined( 'DT_FUNCTIONS_READY' ) ){
+        require get_template_directory() . '/dt-core/global-functions.php';
+    }
+    /*
+     * Don't load the plugin on every rest request. Only those with the metrics namespace
+     */
+    $is_rest = dt_is_rest();
+    if ( !$is_rest || strpos( dt_get_url_path(), 'dt_starter' ) != false ){
         return DT_Starter_Plugin::get_instance();
     }
-    else {
-        add_action( 'admin_notices', 'dt_starter_plugin_hook_admin_notice' );
-        add_action( 'wp_ajax_dismissed_notice_handler', 'dt_starter_plugin_ajax_notice_handler' );
-        return new WP_Error( 'current_theme_not_dt', 'Disciple Tools Theme not active.' );
-    }
-
 }
 add_action( 'plugins_loaded', 'dt_starter_plugin' );
 
@@ -170,22 +184,26 @@ class DT_Starter_Plugin {
      */
     private function setup_actions() {
 
-        // Check for plugin updates
-        if ( ! class_exists( 'Puc_v4p5_Factory' ) ) {
-            require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+        if ( is_admin() ){
+            // Check for plugin updates
+            if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+            /**
+             * Below is the publicly hosted .json file that carries the version information. This file can be hosted
+             * anywhere as long as it is publicly accessible. You can download the version file listed below and use it as
+             * a template.
+             * Also, see the instructions for version updating to understand the steps involved.
+             * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
+             */
+//            @todo enable this section with your own hosted file
+//            $hosted_json = "https://raw.githubusercontent.com/DiscipleTools/disciple-tools-version-control/master/disciple-tools-starter-plugin-version-control.json";
+//            Puc_v4_Factory::buildUpdateChecker(
+//                $hosted_json,
+//                __FILE__,
+//                'disciple-tools-starter-plugin'
+//            );
         }
-        /**
-         * Below is the publicly hosted .json file that carries the version information. This file can be hosted
-         * anywhere as long as it is publicly accessible. You can download the version file listed below and use it as
-         * a template.
-         * Also, see the instructions for version updating to understand the steps involved.
-         * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
-         */
-        Puc_v4p5_Factory::buildUpdateChecker(
-            'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-version-control/master/disciple-tools-starter-plugin-version-control.json',
-            __FILE__,
-            'disciple-tools-starter-plugin'
-        );
 
         // Internationalize the text strings used.
         add_action( 'plugins_loaded', array( $this, 'i18n' ), 2 );
@@ -284,110 +302,14 @@ class DT_Starter_Plugin {
 register_activation_hook( __FILE__, [ 'DT_Starter_Plugin', 'activation' ] );
 register_deactivation_hook( __FILE__, [ 'DT_Starter_Plugin', 'deactivation' ] );
 
-/**
- * Admin alert for when Disciple Tools Theme is not available
- */
-function dt_starter_plugin_no_disciple_tools_theme_found()
-{
-    ?>
-    <div class="notice notice-error">
-        <p><?php esc_html_e( "'Disciple Tools - Starter Plugin' requires 'Disciple Tools' theme to work. Please activate 'Disciple Tools' theme or deactivate 'Disciple Tools - Starter Plugin' plugin.", "dt_starter_plugin" ); ?></p>
-    </div>
-    <?php
-}
-
-/**
- * A simple function to assist with development and non-disruptive debugging.
- * -----------
- * -----------
- * REQUIREMENT:
- * WP Debug logging must be set to true in the wp-config.php file.
- * Add these definitions above the "That's all, stop editing! Happy blogging." line in wp-config.php
- * -----------
- * define( 'WP_DEBUG', true ); // Enable WP_DEBUG mode
- * define( 'WP_DEBUG_LOG', true ); // Enable Debug logging to the /wp-content/debug.log file
- * define( 'WP_DEBUG_DISPLAY', false ); // Disable display of errors and warnings
- * @ini_set( 'display_errors', 0 );
- * -----------
- * -----------
- * EXAMPLE USAGE:
- * (string)
- * write_log('THIS IS THE START OF MY CUSTOM DEBUG');
- * -----------
- * (array)
- * $an_array_of_things = ['an', 'array', 'of', 'things'];
- * write_log($an_array_of_things);
- * -----------
- * (object)
- * $an_object = new An_Object
- * write_log($an_object);
- */
-if ( !function_exists( 'dt_write_log' ) ) {
-    /**
-     * A function to assist development only.
-     * This function allows you to post a string, array, or object to the WP_DEBUG log.
-     *
-     * @param $log
-     */
-    function dt_write_log( $log )
-    {
-        if ( true === WP_DEBUG ) {
-            if ( is_array( $log ) || is_object( $log ) ) {
-                error_log( print_r( $log, true ) );
-            } else {
-                error_log( $log );
-            }
-        }
-    }
-}
-
-if ( ! function_exists( 'dt_is_child_theme_of_disciple_tools' ) ) {
-    /**
-     * Returns true if this is a child theme of Disciple Tools, and false if it is not.
-     *
-     * @return bool
-     */
-    function dt_is_child_theme_of_disciple_tools() : bool {
-        if ( get_template_directory() !== get_stylesheet_directory() ) {
-            $current_theme = wp_get_theme();
-            if ( 'disciple-tools-theme' == $current_theme->get( 'Template' ) ) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
 function dt_starter_plugin_hook_admin_notice() {
-    // Check if it's been dismissed...
-    if ( ! get_option( 'dismissed-dt-starter', false ) ) {
-        // multiple dismissible notice states ?>
-        <div class="notice notice-error notice-dt-starter is-dismissible" data-notice="dt-demo">
-            <p><?php esc_html_e( "'Disciple Tools - Starter Plugin' requires 'Disciple Tools' theme to work. Please activate 'Disciple Tools' theme or deactivate 'Disciple Tools - Starter Plugin'." ); ?></p>
-        </div>
-        <script>
-            jQuery(function($) {
-                $( document ).on( 'click', '.notice-dt-starter .notice-dismiss', function () {
-                    let type = $( this ).closest( '.notice-dt-starter' ).data( 'notice' );
-                    $.ajax( ajaxurl,
-                        {
-                            type: 'POST',
-                            data: {
-                                action: 'dismissed_notice_handler',
-                                type: type,
-                            }
-                        } );
-                } );
-            });
-        </script>
-
-    <?php }
+    global $dt_starter_required_dt_theme_version;
+    $wp_theme = wp_get_theme();
+    $current_version = $wp_theme->version;
+    $message = __( "'Disciple Tools - Starter Plugin' plugin requires 'Disciple Tools' theme to work. Please activate 'Disciple Tools' theme or make sure it is latest version.", "dt_starter" );
+    if ( $wp_theme->get_template() === "disciple-tools-theme" ){
+        $message .= sprintf( esc_html__( 'Current Disciple Tools version: %1$s, required version: %2$s', 'dt_starter' ), esc_html( $current_version ), esc_html( $dt_starter_required_dt_theme_version ) );
+    }
+    dt_hook_admin_notice( $message, 'dt-starter' );
 }
 
-/**
- * AJAX handler to store the state of dismissible notices.
- */
-function dt_starter_plugin_ajax_notice_handler() {
-    $type = 'dt-starter';
-    update_option( 'dismissed-' . $type, true );
-}
