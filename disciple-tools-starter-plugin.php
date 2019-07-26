33a7f76e6ef177a1a10a31ef7d43a6c849a034c1
corsacca <corsacca@gmail.com>
2019-07-26 14:37:48 +0100

    Fix theme check

--- disciple-tools-starter-plugin.php
+++ disciple-tools-starter-plugin.php
@@ -64,7 +64,8 @@ function dt_starter_plugin() {
     /*
      * Check if the Disciple.Tools theme is loaded and is the latest required version
      */
-    if ( 'disciple-tools-theme' !== $wp_theme->get_template() || $version < $dt_starter_required_dt_theme_version ) {
+    $is_theme_dt = strpos( $wp_theme->get_template(), "disciple-tools-theme" ) !== false || $wp_theme->name === "Disciple Tools";
+    if ( !$is_theme_dt || version_compare( $version,  $dt_starter_required_dt_theme_version, "<" ) ) {
         add_action( 'admin_notices', 'dt_starter_plugin_hook_admin_notice' );
         add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
         return new WP_Error( 'current_theme_not_dt', 'Disciple Tools Theme not active or not latest version.' );
