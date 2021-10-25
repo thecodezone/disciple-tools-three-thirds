e91e06297f4b251ecf261b0ab05aa9d1da425c12
corsacca <corsacca@gmail.com>
2021-10-25 14:01:04 +0100

    Update endpoints example file

--- rest-api/rest-api.php
+++ rest-api/rest-api.php
@@ -23,7 +23,7 @@ class Disciple_Tools_Plugin_Starter_Template_Endpoints
 
         register_rest_route(
             $namespace, '/endpoint', [
-                'methods'  => WP_REST_Server::CREATABLE,
+                'methods'  => "GET",
                 'callback' => [ $this, 'endpoint' ],
                 'permission_callback' => function( WP_REST_Request $request ) {
                     return $this->has_permission();
