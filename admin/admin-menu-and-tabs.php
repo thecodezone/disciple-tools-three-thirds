6632195b93f9a17ca0209f54326632b17590a1a6
Chris Chasm <chris@chasm.solutions>
2021-12-29 19:13:26 -0700

    Added echo for var

--- admin/admin-menu-and-tabs.php
+++ admin/admin-menu-and-tabs.php
@@ -73,7 +73,7 @@ class Disciple_Tools_Plugin_Starter_Template_Menu {
 
         ?>
         <div class="wrap">
-            <h2>Plugin Starter Template</h2>
+            <h2><?php echo esc_html( $this->page_title ) ?></h2>
             <h2 class="nav-tab-wrapper">
                 <a href="<?php echo esc_attr( $link ) . 'general' ?>"
                    class="nav-tab <?php echo esc_html( ( $tab == 'general' || !isset( $tab ) ) ? 'nav-tab-active' : '' ); ?>">General</a>
